fd667e7ed04fabf4d3969dbecc5f5f9e9971c794
David <dfl8@aluno.ifal.edu.br>
2024-05-01 00:58:52 -0300

    feat(server): add setErrorHandler with fastify

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,15 +1,28 @@
 import Fastify from 'fastify';
+import { AppError } from '../utils/AppError';
 
-const server = Fastify({
+const fastify = Fastify({
     logger: true
 })
 
 export async function start() {
-    try {
-        await server.listen({ port: 3333 })
-    } catch (error) {
-        throw error
-    }
+    fastify.setErrorHandler((error, request, reply) => {
+        if (error instanceof AppError) {
+            return reply.code(error.statusCode).send({
+                type: "App error",
+                message: error.message
+            })
+        } else {
+            console.log(error);
+
+            return reply.code(500).send({
+                type: "Server error",
+                message: "An error ocurred with the application server.\n Contact the support for help."
+            })
+        }
+    })
+
+    await fastify.listen({ port: 3333})
 }
 
 start();
