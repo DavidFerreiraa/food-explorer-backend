a84b1ce0daa15369a6013e989411feb2e7118671
David <dfl8@aluno.ifal.edu.br>
2024-04-30 22:56:49 -0300

    fix: using now typescript

--- README.md
+++
@@ -1,2 +0,0 @@
-# food-explorer-backend
-Project for the Rocketseat explorer - Final Challenge!
---
+++ api/src/server.ts
@@ -0,0 +1,15 @@
+import Fastify from 'fastify'
+
+const server = Fastify({
+    logger: true
+})
+
+export async function start() {
+    try {
+        await server.listen({ port: 3333 })
+    } catch (error) {
+        throw error
+    }
+}
+
+start();
