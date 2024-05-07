c354fd6e9f5bd848b0861688f671eba41475805c
David <dfl8@aluno.ifal.edu.br>
2024-05-07 16:30:00 -0300

    feat(routes): add categories routes

---
+++ api/src/routes/categories.routes.ts
@@ -0,0 +1,10 @@
+import { FastifyInstance } from "fastify";
+import { CategoriesController } from "../controllers/CategoriesController";
+import { authenticated } from "../middleware/authenticated";
+import { authorized } from "../middleware/authorized";
+
+export async function categoriesRoutes(fastify: FastifyInstance) {
+    const categoriesController = new CategoriesController();
+
+    fastify.post("/categories", {preHandler: [authenticated, authorized(["ADMIN"])]}, async (request, reply) => await categoriesController.create(request, reply));
+}
