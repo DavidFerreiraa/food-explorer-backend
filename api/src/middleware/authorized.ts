b0ef9b253e9a138b4c818e2d5873508100c3de68
David <dfl8@aluno.ifal.edu.br>
2024-05-07 16:31:08 -0300

    fix(middleware): fix the authorized middleware param type

--- api/src/middleware/authorized.ts
+++ api/src/middleware/authorized.ts
@@ -2,10 +2,10 @@ import { $Enums } from "@prisma/client";
 import { FastifyRequest, FastifyReply, HookHandlerDoneFunction } from "fastify";
 import { AppError } from "../../utils/AppError";
 
-export function authorized(roleToVerify: $Enums.Role): (request: FastifyRequest, reply: FastifyReply, done: HookHandlerDoneFunction) => void {
+export function authorized(roleToVerify: string[]): (request: FastifyRequest, reply: FastifyReply, done: HookHandlerDoneFunction) => void {
     return (request: FastifyRequest, reply: FastifyReply, done: HookHandlerDoneFunction) => {
         const { role } = request.user;
-
+        console.log(role)
         if (!roleToVerify.includes(role)) {
             throw new AppError({ message: "Unauthorized", statusCode: 401});
         }
