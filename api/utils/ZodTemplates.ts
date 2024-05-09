019eaad66a0dd18e5ab682a52ef567eb2766a651
David <dfl8@aluno.ifal.edu.br>
2024-05-09 16:01:38 -0300

    feat(utils): add createOrderBody & createOrderId.

--- api/utils/ZodTemplates.ts
+++ api/utils/ZodTemplates.ts
@@ -15,7 +15,7 @@ export const createProductBody = z.object({
     title: z.string({required_error: "You forgot to insert a title"}),
     description: z.string({required_error: "You forgot to insert a description"}),
     price: z.string().default("0"),
-    categoryId: z.string({required_error: "You forgot to insert a category Id"}),
+    categoryId: z.string({required_error: "You forgot to insert a category id"}),
     ingredients: z.array<z.ZodString>(z.string(), {required_error: "Insert at least one ingredient"})
 })
 
@@ -24,14 +24,23 @@ export const createCategoryBody = z.object({
 })
 
 export const createProductImageParams = z.object({
-    productId: z.string({required_error: "You forgot to insert the productId"}),
+    productId: z.string({required_error: "You forgot to insert the product id"}),
     productImageUrl: z.string({required_error: "You forgot to insert the image url"})
 })
 
 export const createProductId = z.object({
-    productId: z.string({required_error: "You forgot to insert the productId"}),
+    productId: z.string({required_error: "You forgot to insert the product id"}),
 })
 
 export const createProductImageUrl = z.object({
     productImageUrl: z.string({required_error: "You forgot to insert the image url"})
+})
+
+export const createOrderBody = z.object({
+    totalPrice: z.number({required_error: "You forgot to insert the total price"}),
+    quantity: z.number({required_error: "You forgot to insert the quantity"})
+});
+
+export const createOrderId = z.object({
+    orderId: z.string({required_error: "You forgot to insert the order id"}),
 })
