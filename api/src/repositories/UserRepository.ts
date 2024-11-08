f07002fbd81335343d164be797a6e3a605928032
David Ferreira Lima <dfl8@aluno.ifal.edu.br>
2024-11-07 22:52:53 -0300

    feat: add setRefreshToken and findByRefreshToken functions

--- api/src/repositories/UserRepository.ts
+++ api/src/repositories/UserRepository.ts
@@ -32,4 +32,24 @@ export class UserRepository {
 
         return user;
     }
+
+    // Updates the user refresh token
+    async setRefreshToken(userId: string, refreshToken: string): Promise<User | null> {
+        const updatedUser = await prisma.user.update({
+            where: { id: userId },
+            data: {
+                refreshToken: refreshToken,  // Armazenando o refreshToken no campo do usuário
+            },
+        });
+
+        return updatedUser;
+    }
+
+    async findByRefreshToken(refreshToken: string): Promise<User | null> {
+        const user = await prisma.user.findFirst({
+            where: { refreshToken: refreshToken },
+        });
+
+        return user;
+    }
 }
