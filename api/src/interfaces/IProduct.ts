c3ab19fc504ef9fb0249ae6264d1d5f32dd9c74d
David Ferreira Lima <dfl8@aluno.ifal.edu.br>
2024-11-14 20:29:14 -0300

    refactor: add all fields that can be needed to IProduct

--- api/src/interfaces/IProduct.ts
+++ api/src/interfaces/IProduct.ts
@@ -1,10 +1,23 @@
 import { Decimal } from "@prisma/client/runtime/library";
 
+interface CategoriesInProducts {
+    categoryId: string,
+    createdAt: string,
+    productId: string,
+    updatedAt: string
+}
+
 export interface IProduct {
+    id?: string,
     title: string,
     description: string,
     price: string | Decimal,
     ingredients: string[],
+    Categories?: CategoriesInProducts[],
+    imageUrl?: string,
     creatorId: string,
+    favoritesId?: string | null,
+    createdAt?: string,
+    updatedAt?: string,
     imageFile?: string
 }
