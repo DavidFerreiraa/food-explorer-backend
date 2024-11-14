65ac0a3246df225412d5032b6c32d84e5b7b6873
David Ferreira Lima <dfl8@aluno.ifal.edu.br>
2024-11-14 20:30:30 -0300

    feat: add put route to routes

--- api/src/routes/products.routes.ts
+++ api/src/routes/products.routes.ts
@@ -13,6 +13,7 @@ export async function productsRoutes(fastify: FastifyInstance) {
     fastify.post("/products", {preHandler: [authenticated, authorized(["ADMIN"]), upload.single("productImage")]}, async (request, reply) => await productsController.create(request as FastifyRequest<{Body: IBody}>, reply));
     fastify.get("/products", {preHandler: [authenticated]}, async (request, reply) => await productsController.index(request as FastifyRequest<{Body: IBody}>, reply));
     fastify.patch("/products/:productId", {preHandler: [authenticated, authorized(["ADMIN"]), upload.single("productImage")]}, async (request, reply) => await productsController.updateImage(request, reply));
+    fastify.put("/products/:productId", {preHandler: [authenticated, authorized(["ADMIN"]), upload.single("productImage")]}, async (request, reply) => await productsController.update(request as FastifyRequest<{Body: IBody}>, reply));
     fastify.get("/products/:productId", {preHandler: [authenticated]}, async (request, reply) => await productsController.show(request, reply));
     fastify.delete("/products/:productId", {preHandler: [authenticated, authorized(["ADMIN"])]}, async (request, reply) => await productsController.delete(request, reply));
 }
